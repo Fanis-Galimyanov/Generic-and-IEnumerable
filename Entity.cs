ff7c2f64a159c1faf39c8690f34a304472e67ec9
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-03-29 01:07:38 +0300

    Add classes Entitiy, Run_IEnumerable

---
+++ Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace Namespace_IEnumerable
+{
+    internal class Entity
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+    }
+}

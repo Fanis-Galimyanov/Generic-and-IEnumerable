fa6d8b9e0d49723b65259871f86083104f753717
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2022-03-28 21:35:29 +0300

    Add project files.

---
+++ LocalFileLogger.cs
@@ -0,0 +1,28 @@
+﻿ internal class LocalFileLogger<T> : ILogger
+    {
+        private readonly string _path;
+        internal LocalFileLogger(string path)
+        {
+            _path = path;
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            WriteToFile($"[Error]: [{typeof(T).Name}] : {message} . {ex.Message}");
+        }
+        public void LogInfo(string message)
+        {
+            WriteToFile($"[Info]: [{typeof(T).Name}] : {message}"); 
+        }
+        public void LogWarning(string message)
+        {
+            WriteToFile($"[Warning]: [{typeof(T).Name}] : {message}");
+        }
+        void WriteToFile(string line)
+        {
+             using (StreamWriter writer = new StreamWriter(_path, true))
+             {
+                writer.WriteLine(line);
+             }
+        }
+}
